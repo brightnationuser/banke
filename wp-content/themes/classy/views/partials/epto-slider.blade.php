b7b7104fee4c52ee6182ceaf4b185617c8223663
Sergii Torkhov <torhov.s@gmail.com>
2019-05-09 13:09:41 +0300

    images

--- wp-content/themes/classy/views/partials/epto-slider.blade.php
+++ wp-content/themes/classy/views/partials/epto-slider.blade.php
@@ -1,5 +1,145 @@
 <div class="epto__bg">
 </div>
-<div class="epto__image">
-    <img src="/wp-content/themes/classy/images/pages/home/epto.png">
+
+{{--<div class="epto__image">--}}
+    {{--<img src="/wp-content/themes/classy/images/pages/home/epto.png">--}}
+{{--</div>--}}
+
+<div id="scene_4">
+
+    <div class="epto-w">
+        <img src="/wp-content/themes/classy/images/pages/home/epto.png" class="epto__image preload">
+
+        <div class="infobox-w" id="infobox_epto_0">
+            <div class="infobox__dot"><span></span><span></span><span></span></div>
+
+            <div class="infobox__content" style="display: none">
+                <div class="content__image">
+                    <img src="/wp-content/themes/classy/images/pages/home/epto/epto_0.png" class="preload">
+                </div>
+                <div class="content__text">
+                    The on-board charger is connected to the mains using an industrial standard CEE-16A connector.  You can recharge on any location where 400 Volt 3 phase (16 Amp group) is available
+                </div>
+            </div>
+        </div>
+
+        <div class="infobox-w" id="infobox_epto_1">
+            <div class="infobox__dot"><span></span><span></span><span></span></div>
+
+            <div class="infobox__content" style="display: none">
+                <div class="content__image">
+                    <img src="/wp-content/themes/classy/images/pages/home/epto/epto_1.png" class="preload">
+                </div>
+                <div class="content__text">
+                    Lithium Ion batteries ensure low weight of the E-PTO. The chemistry used (LiFePO4) ensures a slow aging of the batteries when these are deeply discharged on a daily basis
+                </div>
+            </div>
+        </div>
+
+        <div class="infobox-w" id="infobox_epto_2">
+            <div class="infobox__dot"><span></span><span></span><span></span></div>
+
+            <div class="infobox__content" style="display: none">
+                <div class="content__image">
+                    <img src="/wp-content/themes/classy/images/pages/home/epto/epto_2.png" class="preload">
+                </div>
+                <div class="content__text">
+                    One package that contains all components. No external high voltage cables
+                </div>
+            </div>
+        </div>
+
+        <div class="infobox-w" id="infobox_epto_3">
+            <div class="infobox__dot"><span></span><span></span><span></span></div>
+
+            <div class="infobox__content" style="display: none">
+                <div class="content__image">
+                    <img src="/wp-content/themes/classy/images/pages/home/epto/epto_3.png" class="preload">
+                </div>
+                <div class="content__text">
+                    Every E-PTO can be delivered with Load Sense of fixed single or double chamber pump. Banke however advises to use our special fixed pump that is close to noiseless
+                </div>
+            </div>
+        </div>
+
+        <div class="infobox-w" id="infobox_epto_4">
+            <div class="infobox__dot"><span></span><span></span><span></span></div>
+
+            <div class="infobox__content" style="display: none">
+                <div class="content__image">
+                    <img src="/wp-content/themes/classy/images/pages/home/epto/epto_4.png" class="preload">
+                </div>
+                <div class="content__text">
+                    Available with CAN-bus or Analog control interface
+                </div>
+            </div>
+        </div>
+
+        <div class="infobox-w" id="infobox_epto_5">
+            <div class="infobox__dot"><span></span><span></span><span></span></div>
+
+            <div class="infobox__content" style="display: none">
+                <div class="content__image">
+                    <img src="/wp-content/themes/classy/images/pages/home/epto/epto_5.png" class="preload">
+                </div>
+                <div class="content__text">
+                    30 KW nominal and 45 kW peak performance
+                </div>
+            </div>
+        </div>
+
+        <div class="infobox-w" id="infobox_epto_6">
+            <div class="infobox__dot"><span></span><span></span><span></span></div>
+
+            <div class="infobox__content" style="display: none">
+                <div class="content__image">
+                    <img src="/wp-content/themes/classy/images/pages/home/epto/epto_6.png" class="preload">
+                </div>
+                <div class="content__text">
+                    Available in 18, 36, 54 and 72 kWh capacity
+                </div>
+            </div>
+        </div>
+
+        <div class="infobox-w" id="infobox_epto_7">
+            <div class="infobox__dot"><span></span><span></span><span></span></div>
+
+            <div class="infobox__content" style="display: none">
+                <div class="content__image">
+                    <img src="/wp-content/themes/classy/images/pages/home/epto/epto_7.png" class="preload">
+                </div>
+                <div class="content__text">
+                    The battery pack is constructed in such way that each cell can be individually exchanged
+                </div>
+            </div>
+        </div>
+
+        <div class="infobox-w" id="infobox_epto_8">
+            <div class="infobox__dot"><span></span><span></span><span></span></div>
+
+            <div class="infobox__content" style="display: none">
+                <div class="content__image">
+                    <img src="/wp-content/themes/classy/images/pages/home/epto/epto_8.png" class="preload">
+                </div>
+                <div class="content__text">
+                    Powder coated aluminium outer structure in RAL 9010 white. Can be delivered in RAL colour code if this colour is available as powder coat. Please consult the factory.
+                </div>
+            </div>
+        </div>
+
+        <div class="infobox-w" id="infobox_epto_9">
+            <div class="infobox__dot"><span></span><span></span><span></span></div>
+
+            <div class="infobox__content" style="display: none">
+                <div class="content__image">
+                    <img src="/wp-content/themes/classy/images/pages/home/epto/epto_9.png" class="preload">
+                </div>
+                <div class="content__text">
+                    Banke E-PTO systems have a generic design and can be used with all body manufacturers
+                </div>
+            </div>
+        </div>
+
+    </div>
+
 </div>
