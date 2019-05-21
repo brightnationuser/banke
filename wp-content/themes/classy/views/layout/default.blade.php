8e6e83eb0215b14d00e14aee2e6dc64e0c6621b0
Sergii Torkhov <torhov.s@gmail.com>
2019-05-21 13:38:43 +0200

    add Analytics

--- wp-content/themes/classy/views/layout/default.blade.php
+++ wp-content/themes/classy/views/layout/default.blade.php
@@ -67,6 +67,17 @@
         {!! get_field('acf_header_code', 'option') !!}
 
         <link rel="stylesheet" href="{{ \Helpers\General::asset_hash('/wp-content/css/app.css') }}">
+
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-131150497-2"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-131150497-2');
+        </script>
+
     </head>
 
     <body {{ body_class($body_additional) }}>
