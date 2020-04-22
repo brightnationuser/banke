63030c89c4a89be4ab548c0ec1341994f18f21c2
alexander <holovko.works@gmail.com>
2020-04-22 18:02:07 +0300

    home part 1

--- wp-content/themes/classy/views/home.blade.php
+++ wp-content/themes/classy/views/home.blade.php
@@ -27,59 +27,24 @@
         </div>
     </div>
 
-    <div class="benefits m_4">
-        <div class="container">
-            @foreach($post->getAcfByKey('acf_header')['acf_header_benefits'] as $key => $item)
-                <div class="benefit">
-                    <div class="benefit__image">
-                        @if($key == 0)<i class="icon-lamp"></i>@endif
-                        @if($key == 1)<i class="icon-earth"></i>@endif
-                        @if($key == 2)<i class="icon-design"></i>@endif
-                        @if($key == 3)<i class="icon-quality"></i>@endif
-                    </div>
-
-                    <div class="benefit__title">
-                        {{ $item['acf_header_benefits_title'] }}
-                    </div>
-
-                    <div class="benefit__caption">
-                        {{ $item['acf_header_benefits_text'] }}
-                    </div>
-                </div>
-            @endforeach
-        </div>
-    </div>
-
-    <div class="our-products">
+    <div class="what-we-do">
         <div class="container">
             <h2>
-                Our products
+                What We Do
             </h2>
 
-            {{--@include('partials.car-slider')--}}
-        </div>
-    </div>
+            <div class="what-we-do__list">
+                <div class="what-we-do__item item">
 
-    <div class="epto">
-        <div class="container">
-            @include('partials.slider.epto-slider')
-
-            <div class="description-w">
-                <h3>
-                    {{ $post->getAcfByKey('acf_our_products')['acf_our_products_title_2'] }}
-                </h3>
-
-                <div class="description__text">
-                    {{ $post->getAcfByKey('acf_our_products')['acf_our_products_text_2'] }}
-                </div>
-
-                <div class="description__button">
-                    <a href="/epto-systems/" class="button">Read more</a>
                 </div>
             </div>
         </div>
     </div>
 
-    @include('partials.contact-us')
+    @include('partials.contact-us', [
+        'form' => $post->getAcfByKey('contact_form'),
+        'title' => 'Contact Us',
+        'classes' => 'contact-us--light'
+    ])
 
 @stop
