c9d0beadddec38640a9372021dd4f915dce36178
alexander holovko <holovko.works@gmail.com>
2020-11-17 19:18:02 +0200

    video gallery add admin

--- wp-content/themes/classy/views/partials/video-gallery.blade.php
+++ wp-content/themes/classy/views/partials/video-gallery.blade.php
@@ -1,51 +1,47 @@
 @php
-    $videos = [
-        [
-            'video_id' => 'LlsAKKd8rrI',
-            'image' => '/wp-content/themes/classy/images/pages/powertrains/video-1.jpg',
-            'text' => 'Step 1'
-        ],
-        [
-            'video_id' => '5n386Sguyz8',
-            'image' => '/wp-content/themes/classy/images/pages/powertrains/video-2.jpg',
-            'text' => 'Step 2'
-        ],
-        [
-            'video_id' => 'LlsAKKd8rrI',
-            'image' => '/wp-content/themes/classy/images/pages/powertrains/video-3.jpg',
-            'text' => 'Step 3'
-        ],
-                [
-            'video_id' => 'LlsAKKd8rrI',
-            'image' => '/wp-content/themes/classy/images/pages/powertrains/video-1.jpg',
-            'text' => 'Step 1'
-        ],
-        [
-            'video_id' => '5n386Sguyz8',
-            'image' => '/wp-content/themes/classy/images/pages/powertrains/video-2.jpg',
-            'text' => 'Step 2'
-        ],
-        [
-            'video_id' => 'LlsAKKd8rrI',
-            'image' => '/wp-content/themes/classy/images/pages/powertrains/video-3.jpg',
-            'text' => 'Step 3'
-        ],
-    ];
+    $videos = get_field('video_gallery');
+
+    if(empty($videos)) {
+        $videos = [
+            [
+                'video_id' => 'LlsAKKd8rrI',
+                'image' => ['url' => '/wp-content/themes/classy/images/pages/powertrains/video-1.jpg'],
+                'text' => 'Step 1'
+            ],
+            [
+                'video_id' => '5n386Sguyz8',
+                'image' => ['url' => '/wp-content/themes/classy/images/pages/powertrains/video-2.jpg'],
+                'text' => 'Step 2'
+            ],
+            [
+                'video_id' => 'LlsAKKd8rrI',
+                'image' => ['url' => '/wp-content/themes/classy/images/pages/powertrains/video-3.jpg'],
+                'text' => 'Step 3'
+            ],
+        ];
+    }
+
+    $videos = array_merge($videos, $videos, $videos);
 
     foreach($videos as $key => &$item) {
         $item['player_id'] = $item['video_id'] . $key;
     }
+
+    $start = get_field('video_gallery_start');
+    $title = get_field('video_gallery_title');
 @endphp
 
 <div class="video-gallery">
     <div class="container">
-        <h2 class="video-gallery__title">Video Gallery</h2>
-        <div class="owl-carousel owl-video_gallery">
+        @if(!empty($title))
+            <h2 class="video-gallery__title">{{ $title }}</h2>
+        @endif
+        <div class="owl-carousel owl-video_gallery" data-start="{{ !empty($start) ? $start : 4 }}">
             @foreach($videos as $video)
                 <div class="video-gallery__item">
                     @include ('partials.embed-youtube-video', [
                         'title' => '',
-                        'image' => $video['image'],
+                        'image' => $video['image']['url'],
                         'yt_id' => $video['video_id'],
                         'index' => $video['player_id'],
                         'no_btn_text' => true
