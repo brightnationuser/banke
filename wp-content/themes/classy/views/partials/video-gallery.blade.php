cc90f5df5a249d810d8d4984c36fb708f5882c47
alexander holovko <holovko.works@gmail.com>
2020-11-19 11:15:31 +0200

    hide if no videos

--- wp-content/themes/classy/views/partials/video-gallery.blade.php
+++ wp-content/themes/classy/views/partials/video-gallery.blade.php
@@ -1,26 +1,6 @@
 @php
     $videos = get_field('video_gallery');
 
-    if(empty($videos)) {
-        $videos = [
-            [
-                'video_id' => 'LlsAKKd8rrI',
-                'image' => ['url' => '/wp-content/themes/classy/images/pages/powertrains/video-1.jpg'],
-                'text' => 'Step 1'
-            ],
-            [
-                'video_id' => '5n386Sguyz8',
-                'image' => ['url' => '/wp-content/themes/classy/images/pages/powertrains/video-2.jpg'],
-                'text' => 'Step 2'
-            ],
-            [
-                'video_id' => 'LlsAKKd8rrI',
-                'image' => ['url' => '/wp-content/themes/classy/images/pages/powertrains/video-3.jpg'],
-                'text' => 'Step 3'
-            ],
-        ];
-    }
-
     $videos = array_merge($videos, $videos, $videos);
 
     foreach($videos as $key => &$item) {
@@ -31,6 +11,7 @@
     $title = get_field('video_gallery_title');
 @endphp
 
+@if(empty($videos))
 <div class="video-gallery">
     <div class="container">
         @if(!empty($title))
@@ -62,3 +43,4 @@
         </div>
     </div>
 </div>
+@endif
