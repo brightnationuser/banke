795d7c27dce1e4ef9fdb4557db98a8287be84ca0
SelectPerson <test@gmail.com>
2021-02-05 12:22:00 +0200

    feature/delete hotjar analytics and added google tag manager

--- wp-content/themes/classy/views/layout/default.blade.php
+++ wp-content/themes/classy/views/layout/default.blade.php
@@ -72,18 +72,17 @@
 
         <?php /* <link rel="stylesheet" href="{{ \Helpers\General::asset_hash('/wp-content/css/app.css') }}"> */ ?>
 
-        <!-- Hotjar Tracking Code for https://banke.pro -->
+        <!-- Google Tag Manager -->
         <script>
-            (function(h,o,t,j,a,r){
-                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                h._hjSettings={hjid:1378254,hjsv:6};
-                a=o.getElementsByTagName('head')[0];
-                r=o.createElement('script');r.async=1;
-                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                a.appendChild(r);
-            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-        </script>
 
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-MVNHP8D');
+
+        </script>
+        <!-- End Google Tag Manager -->
         <!-- Global site tag (gtag.js) - Google Analytics -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=UA-131150497-2"></script>
         <script>
