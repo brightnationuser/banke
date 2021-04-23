7bd3e51219b11fbe8adc7cb582495d3509cd81af
alexander-holovko <holovko.works@gmail.com>
2021-04-23 22:42:53 +0300

    fix

--- wp-content/themes/classy/js/Vue/pages/ThreeModels.vue
+++ wp-content/themes/classy/js/Vue/pages/ThreeModels.vue
@@ -28,11 +28,12 @@
         </div>
         <div class="personal-entities__list" v-if="searchInProcess">
           <PersonalBlock
-              v-for="(elem, index) in data" :key="elem.id"
+              v-for="(elem, index) in data"
+              :key="elem.id"
               v-if="elem.type !== 'video'"
               :title="elem.title"
               :model="elem.type === 'models3d'"
-              :subtitle="translations.titles.models_3d"
+              :subtitle="elem.category.description"
               :image="elem.image.url"
               :files="elem.files"
           />
