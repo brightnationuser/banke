ccf816702d308867700fc89871d3031670983f64
brnteka <mpbrainwave@gmail.com>
2022-02-03 19:22:21 +0200

    menu item becomes active if we are on one of the tabs page

--- wp-content/themes/classy/functions.php
+++ wp-content/themes/classy/functions.php
@@ -3,6 +3,20 @@
  * Init our WordPress Theme.
  */
 //require_once( __DIR__ . '/../../../../vendor/autoload.php' );
-require_once( __DIR__ . '/../../../vendor/autoload.php' );
+require_once(__DIR__ . '/../../../vendor/autoload.php');
 
 \Classy\Classy::get_instance();
+
+add_filter('nav_menu_css_class', 'special_nav_class', 10, 2);
+
+function special_nav_class($classes, $item)
+{
+
+    if (
+        in_array($item->title, array('Produkte', 'Products'))
+        && (is_page('epto-systems') || is_page('the-full-electric-powertrains') || is_page('electric-chassis-pto'))
+    ) {
+        $classes[] = "current-menu-item";
+    }
+    return $classes;
+}
