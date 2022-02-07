28a1434cde75df4ecb143604fc31e0dcd9e5b959
brnteka <mpbrainwave@gmail.com>
2022-02-07 12:23:20 +0200

    fixes news post type language issue

--- wp-content/themes/classy/scope/common.php
+++ wp-content/themes/classy/scope/common.php
@@ -1,23 +1,39 @@
 <?php
+
+global $wp;
+global $sitepress;
+
 /**
  * Data that will be accessible in every view.
  */
 
-
-if( is_single()) {
-    $post_type = get_query_var( 'post_type' );
-    $body_additional = 'p-'.$post_type;
-}
-elseif(is_post_type_archive()) {
-    $post_type = get_query_var( 'post_type' );
-    $body_additional = 'archive-'.$post_type;
-}
-else {
+if (is_single()) {
+    $post_type = get_query_var('post_type');
+    $body_additional = 'p-' . $post_type;
+} elseif (is_post_type_archive()) {
+    $post_type = get_query_var('post_type');
+    $body_additional = 'archive-' . $post_type;
+} else {
     $_post = get_queried_object();
-    $body_additional = 'p-'.$_post->post_name;
+    $body_additional = 'p-' . $_post->post_name;
 }
+
+// because wpml_active_languages won't return any
+// languages at all if the post type is set to not translatable
+// we hand it a dummy array with the default site wide language
+
+$default_language = $sitepress->get_default_language();
+
+$languages = apply_filters('wpml_active_languages', NULL, 'skip_missing=0&orderby=id&order=desc') ?: array(
+    array(
+        'active' => 1,
+        'url' => home_url($wp->request),
+        'language_code' => $default_language
+    )
+);
 
 $data = array(
     //'menu' => new Classy\Menu(),
-    'body_additional' => $body_additional
+    'body_additional' => $body_additional,
+    'languages' => $languages
 );
--- wp-content/themes/classy/views/page/news.blade.php
+++ wp-content/themes/classy/views/page/news.blade.php
@@ -9,7 +9,7 @@
 
         <div class="news-list">
             @foreach($news as $news_item)
-                <a href="{{ $news_item->permalink() }}" class="news">
+                <a href="{{ apply_filters('wpml_permalink', $news_item->permalink(), 'en') }}" class="news">
                     <div class="news__img">
                         <div class="img__inner b-lazy" data-src="{{ $news_item->getAcfImage()->src('large') }}">
                         </div>
