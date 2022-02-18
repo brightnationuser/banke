75f1c9921b8cbac92fafc741fa204a760ef4f28a
brnteka <mpbrainwave@gmail.com>
2022-02-18 14:19:13 +0200

    fixes news page items overflow on mobile

--- wp-content/themes/classy/views/page/news.blade.php
+++ wp-content/themes/classy/views/page/news.blade.php
@@ -7,29 +7,31 @@
 
         <h1 class="h2 h2--mt-0">{!! get_field('news_title', 'options') !!}</h1>
 
-        <div class="news-list">
-            @foreach($news as $news_item)
-                <a href="{{ apply_filters('wpml_permalink', $news_item->permalink(), 'en') }}" class="news">
-                    <div class="news__img">
-                        <div class="img__inner b-lazy" data-src="{{ $news_item->getAcfImage()->src('large') }}">
-                        </div>
-                    </div>
-
-                    <div class="news__content">
-                        <div class="news__date">
-                            {{ $news_item->getDate() }}
+        <div class="news-list-wrapper">
+            <div class="news-list">
+                @foreach($news as $news_item)
+                    <a href="{{ apply_filters('wpml_permalink', $news_item->permalink(), 'en') }}" class="news">
+                        <div class="news__img">
+                            <div class="img__inner b-lazy" data-src="{{ $news_item->getAcfImage()->src('large') }}">
+                            </div>
                         </div>
 
-                        <div class="news__title">
-                            {!! $news_item->title() !!}
-                        </div>
-                    </div>
-                </a>
-            @endforeach
+                        <div class="news__content">
+                            <div class="news__date">
+                                {{ $news_item->getDate() }}
+                            </div>
 
-            <div class="news m_fake"></div>
-            <div class="news m_fake"></div>
-            <div class="news m_fake"></div>
+                            <div class="news__title">
+                                {!! $news_item->title() !!}
+                            </div>
+                        </div>
+                    </a>
+                @endforeach
+
+                <div class="news m_fake"></div>
+                <div class="news m_fake"></div>
+                <div class="news m_fake"></div>
+            </div>
         </div>
 
         {!! $pagination_layout !!}
