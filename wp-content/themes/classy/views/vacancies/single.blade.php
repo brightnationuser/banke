8ddf9a93876b71dedc3d2da96f04a74fedad18af
Oleg <3ejiehuu2@gmail.com>
2022-03-21 12:19:03 +0200

    changed form id for vacancies

--- wp-content/themes/classy/views/vacancies/single.blade.php
+++ wp-content/themes/classy/views/vacancies/single.blade.php
@@ -58,7 +58,8 @@
     @if(!$acf_group['our_team_link'] && !$acf_group['vacancy_closed'])
         <div id="our_team_form" class="modal-team" style="display: none;">
 {{--            {!! do_shortcode('[contact-form-7 id="2493" title="Join Our Team EN"]') !!}--}}
-            {!! do_shortcode('[contact-form-7 id="2487" title="Join Our Team EN"]') !!}
+{{--            {!! do_shortcode('[contact-form-7 id="2487" title="Join Our Team EN"]') !!}--}}
+            {!! do_shortcode('[contact-form-7 id="2572" title="Join Our Team EN"]') !!}
 
             <div class="modal-thank">
                 <img width="55" src="{{get_template_directory_uri()}}/images/icons/icon_email_big.svg" alt="email sent">
