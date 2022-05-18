bdbc0871a781e1c5ce94c75851118e4f57cc6aad
Oleg <3ejiehuu2@gmail.com>
2022-05-18 12:38:04 +0300

    [*] layout fixes, popup button logic fix

--- wp-content/themes/classy/views/partials/popup-anniversary.blade.php
+++ wp-content/themes/classy/views/partials/popup-anniversary.blade.php
@@ -7,14 +7,14 @@
                 <h2 class="popup__title">{{$title}}</h2>
 
                 <div class="popup__info">
-                    <p class="date">{{$date}}</p>
+                    <p class="date">{!! $date !!}</p>
 
                     <p class="time">{{$time}}</p>
 
                     <p class="address">{{$address}}</p>
                 </div>
 
-                <a href="{{$link}}" class="popup__link">Read More</a>
+                <a data-link="{{$link}}" class="popup__link">Read More</a>
             </div>
 
             <button class="popup__close js-popup-close" type="button">&times;</button>
