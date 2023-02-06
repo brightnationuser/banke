34e31e887d325c152a6b5a730eddc4ac02551781
sergeymad <LittleOne#21>
2023-02-06 14:42:20 +0200

    [+] moved logic to functions.php

--- wp-content/themes/classy/app/custom/functions.php
+++ wp-content/themes/classy/app/custom/functions.php
@@ -1,6 +1,23 @@
 <?php
 use \Helpers\Kama_Breadcrumbs;
 
+function autodetect_language(){
+    global $wp,$sitepress;
+    $default_language = $sitepress->get_default_language();
+    $languages = apply_filters('wpml_active_languages', NULL, 'skip_missing=0&orderby=id&order=desc') ?: array(
+        array(
+            'active' => 1,
+            'url' => home_url($wp->request),
+            'language_code' => $default_language
+        )
+    );
+
+    if((strripos($_SERVER['HTTP_ACCEPT_LANGUAGE'],"de")!==false || strripos($_SERVER['HTTP_ACCEPT_LANGUAGE'],"de_DE")!==false) && strripos($_SERVER["REQUEST_URI"],"/de/") === false && !isset($_COOKIE['language_checked']) ){
+        setcookie('language_checked',true);
+        header('Location: '.$languages['de']['url']);
+    }
+}
+autodetect_language();
 function kama_breadcrumbs( $sep = '>', $l10n = array(), $args = array() ){
     $kb = new Kama_Breadcrumbs;
     echo $kb->get_crumbs( $sep, $l10n, $args );
--- wp-content/themes/classy/header.php
+++ wp-content/themes/classy/header.php
@@ -2,19 +2,6 @@
 /**
  * Theme header.
  */
-global $wp,$sitepress;
-$default_language = $sitepress->get_default_language();
-$languages = apply_filters('wpml_active_languages', NULL, 'skip_missing=0&orderby=id&order=desc') ?: array(
-    array(
-        'active' => 1,
-        'url' => home_url($wp->request),
-        'language_code' => $default_language
-    )
-);
 
-if((strripos($_SERVER['HTTP_ACCEPT_LANGUAGE'],"de")!==false || strripos($_SERVER['HTTP_ACCEPT_LANGUAGE'],"de_DE")!==false) && strripos($_SERVER["REQUEST_URI"],"/de/") === false && !isset($_COOKIE['language_checked']) ){
-    setcookie('language_checked',true);
-    header('Location: '.$languages['de']['url']);
-}
 $framework = get_theme_framework();
 $framework::render( 'layout.header' );
