6671afc8578fb411f7ef8cb55a2de4dc81a374aa
sergeymad <LittleOne#21>
2023-02-06 11:44:39 +0200

    [+] changed locale code

--- wp-content/themes/classy/header.php
+++ wp-content/themes/classy/header.php
@@ -11,8 +11,8 @@ $languages = apply_filters('wpml_active_languages', NULL, 'skip_missing=0&orderb
         'language_code' => $default_language
     )
 );
-
-if((strripos($_SERVER['HTTP_ACCEPT_LANGUAGE'],"de_DE")!==false || strripos($_SERVER['HTTP_ACCEPT_LANGUAGE'],"de_DE")!==false) && strripos($_SERVER["REQUEST_URI"],"/de/") === false && !isset($_COOKIE['language_checked']) ){
+var_dump($_SERVER['HTTP_ACCEPT_LANGUAGE']);
+if((strripos($_SERVER['HTTP_ACCEPT_LANGUAGE'],"de")!==false || strripos($_SERVER['HTTP_ACCEPT_LANGUAGE'],"de_DE")!==false) && strripos($_SERVER["REQUEST_URI"],"/de/") === false && !isset($_COOKIE['language_checked']) ){
     setcookie('language_checked',true);
     header('Location: '.$languages['de']['url']);
 }
