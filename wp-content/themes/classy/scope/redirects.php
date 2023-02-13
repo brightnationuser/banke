97832100b60b630ebaf3f0adf6f9bec9bf6e479d
sergeymad <LittleOne#21>
2023-02-13 15:02:43 +0200

    [+] changed redirect place

--- wp-content/themes/classy/scope/redirects.php
+++ wp-content/themes/classy/scope/redirects.php
@@ -14,3 +14,17 @@ foreach($relations as $key => $val){
         exit();
     }
 }
+global $wp,$sitepress;
+$default_language = $sitepress->get_default_language();
+$languages = apply_filters('wpml_active_languages', NULL, 'skip_missing=0&orderby=id&order=desc') ?: array(
+    array(
+        'active' => 1,
+        'url' => home_url($wp->request),
+        'language_code' => $default_language
+    )
+);
+
+if((strripos($_SERVER['HTTP_ACCEPT_LANGUAGE'],"de")!==false || strripos($_SERVER['HTTP_ACCEPT_LANGUAGE'],"de_DE")!==false) && strripos($_SERVER["REQUEST_URI"],"/de/") === false && !isset($_COOKIE['language_checked']) ){
+    setcookie('language_checked',true);
+    header('Location: '.$languages['de']['url']);
+}
