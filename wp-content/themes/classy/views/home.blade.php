d20a989b13b54631e5951734766735b7fda1a443
sergeymad <LittleOne#21>
2023-02-13 12:14:15 +0200

    [+] changed related section title

--- wp-content/themes/classy/views/home.blade.php
+++ wp-content/themes/classy/views/home.blade.php
@@ -69,9 +69,9 @@
 
     <div class="home-references">
         <div class="container">
-            @if(!empty(get_field('references_title')))
+            @if(!empty(get_field('case_study_title')))
                 <h2>
-                    {{ get_field('references_title') }}
+                    {{ get_field('case_study_title') }}
                 </h2>
                 @if(!empty(get_field('references_about')))
                     <p>{{ get_field('references_about') }}</p>
