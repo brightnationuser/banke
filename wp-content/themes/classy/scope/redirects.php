a807d0e870a59abdb3340438c3aa3c6d350c87a3
sergeymad <LittleOne#21>
2023-02-14 11:01:16 +0200

    [+] moved redirect to top of the file

--- wp-content/themes/classy/scope/redirects.php
+++ wp-content/themes/classy/scope/redirects.php
@@ -1,4 +1,18 @@
 <?php
+global $wp,$sitepress;
+$default_language = $sitepress->get_default_language();
+$languages = apply_filters('wpml_active_languages', NULL, 'skip_missing=0&orderby=id&order=desc') ?: array(
+    array(
+        'active' => 1,
+        'url' => home_url($wp->request),
+        'language_code' => $default_language
+    )
+);
+
+if((strripos($_SERVER['HTTP_ACCEPT_LANGUAGE'],"de")!==false || strripos($_SERVER['HTTP_ACCEPT_LANGUAGE'],"de_DE")!==false) && strripos($_SERVER["REQUEST_URI"],"/de/") === false && !isset($_COOKIE['language_checked']) ){
+    setcookie('language_checked',true);
+    header('Location: '.$languages['de']['url']);
+}
 $relations = [
     'epto-systems/references' => 'epto-systems/',
     'epto-systems/media' => 'epto-systems/',
@@ -13,18 +27,4 @@ foreach($relations as $key => $val){
         header("Location: http://". $_SERVER['SERVER_NAME'] . '/' . $val);
         exit();
     }
-}
-global $wp,$sitepress;
-$default_language = $sitepress->get_default_language();
-$languages = apply_filters('wpml_active_languages', NULL, 'skip_missing=0&orderby=id&order=desc') ?: array(
-    array(
-        'active' => 1,
-        'url' => home_url($wp->request),
-        'language_code' => $default_language
-    )
-);
-
-if((strripos($_SERVER['HTTP_ACCEPT_LANGUAGE'],"de")!==false || strripos($_SERVER['HTTP_ACCEPT_LANGUAGE'],"de_DE")!==false) && strripos($_SERVER["REQUEST_URI"],"/de/") === false && !isset($_COOKIE['language_checked']) ){
-    setcookie('language_checked',true);
-    header('Location: '.$languages['de']['url']);
 }
