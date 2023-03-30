bab6209af597d4d5f4359feaab59e18dcac2d6e5
sergeymad <LittleOne#21>
2023-03-30 14:13:17 +0300

    [+] refactoring

--- wp-content/themes/classy/views/case_studies/single.blade.php
+++ wp-content/themes/classy/views/case_studies/single.blade.php
@@ -21,7 +21,7 @@
         <div class="container">
             <h2 class="case__solution__title">Solution</h2>
             <div class="case__solution__text">{!! get_field('solution') !!}</div>
-            <a href="{!! get_permalink( $id ) !!}"
+            <a href="{!! get_permalink(get_field('product')) !!}"
                class="case__solution__button">{!! get_field('case_studies_learn_more_button', 'option') !!}</a>
         </div>
     </div>
