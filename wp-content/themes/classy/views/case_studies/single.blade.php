2034ef4f9905d56fe440389c5bc055220daf735f
sergeymad <LittleOne#21>
2023-04-04 13:25:34 +0300

    [+] removed unnecessary callings

--- wp-content/themes/classy/views/case_studies/single.blade.php
+++ wp-content/themes/classy/views/case_studies/single.blade.php
@@ -19,14 +19,15 @@
     </article>
 
     @if(!empty(get_field('solution')))
-    <div class="case__solution" style="background-image: url(/wp-content/themes/classy/images/case-solutions-bg.jpg)">
-        <div class="container">
-            <h2 class="case__solution__title">{!! get_field('solutions_title', 'option') !!}</h2>
-            <div class="case__solution__text">{!! get_field('solution') !!}</div>
-            <a href="{!! get_permalink(get_field('product')) !!}"
-               class="case__solution__button">{!! get_field('case_studies_learn_more_button', 'option') !!}</a>
+        <div class="case__solution"
+             style="background-image: url(/wp-content/themes/classy/images/case-solutions-bg.jpg)">
+            <div class="container">
+                <h2 class="case__solution__title">{!! get_field('solutions_title', 'option') !!}</h2>
+                <div class="case__solution__text">{!! get_field('solution') !!}</div>
+                <a href="{!! get_permalink(get_field('product')) !!}"
+                   class="case__solution__button">{!! get_field('case_studies_learn_more_button', 'option') !!}</a>
+            </div>
         </div>
-    </div>
     @endif
 
     @if(get_field('specification_enabled'))
@@ -53,10 +54,12 @@
                         @if(get_field('file')['url'] != "")
                             <a class="case__download" href="{!! get_field('file')['url']  !!}" target="_blank"
                                download="{!! get_field('file')['url']  !!}">
-                                <i class="icon-down-arrow"></i> <span>{!! get_field('case_study_specification', 'option') !!}</span> </a>
+                                <i class="icon-down-arrow"></i>
+                                <span>{!! get_field('case_study_specification', 'option') !!}</span> </a>
                         @else
                             <a class="case__download" href="#" style="pointer-events: none">
-                                <i class="icon-down-arrow"></i> <span>{!! get_field('case_study_specification', 'option') !!}</span> </a>
+                                <i class="icon-down-arrow"></i>
+                                <span>{!! get_field('case_study_specification', 'option') !!}</span> </a>
                         @endif
                     </div>
                 </div>
@@ -65,23 +68,28 @@
     @endif
 
     @if(!empty(get_field('product_post',$case_studies_item->ID)))
-    <div class="case__product">
-        <div class="case__specifications__container">
-            <div class="wrapper">
-                <div class="image">
+
+        @php
+            $product_post = get_field('product_post')
+        @endphp
+
+        <div class="case__product">
+            <div class="case__specifications__container">
+                <div class="wrapper">
+                    <div class="image">
                         <img src="{!! get_field('product')['url'];!!}" alt="case icon">
-                </div>
-                <div class="text">
-                    <h2 class="case__solution__title">
-                        {!!  get_field('product_post')->post_title !!}
-                    </h2>
-                    <div class="case__solution__text">{!!  get_field('case_studies_info', get_field('product_post')->ID) !!}</div>
-                    <a href="{!! get_permalink( get_field('product_post')->ID ) !!}"
-                       class="button button--primary disable_preloader">{!! get_field('case_studies_learn_more_button', 'option') !!}</a>
+                    </div>
+                    <div class="text">
+                        <h2 class="case__solution__title">
+                            {!!  $product_post->post_title !!}
+                        </h2>
+                        <div class="case__solution__text">{!!  get_field('case_studies_info', $product_post->ID) !!}</div>
+                        <a href="{!! get_permalink( $product_post->ID ) !!}"
+                           class="button button--primary disable_preloader">{!! get_field('case_studies_learn_more_button', 'option') !!}</a>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
     @endif
 
     @include('partials.related-cases')
