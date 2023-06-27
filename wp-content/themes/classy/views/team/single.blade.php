d6715ee4ebc7050d92601bb456cc3efaee3c41df
brnteka <mpbrainwave@gmail.com>
2023-06-27 16:06:18 +0300

    [*] breadcrumbs single team page

--- wp-content/themes/classy/views/team/single.blade.php
+++ wp-content/themes/classy/views/team/single.blade.php
@@ -5,6 +5,23 @@
         <div class="container">
             <div class="article">
                 <div class="article__content">
+
+                    @php
+                        $parent_id = get_page_by_path( 'meet-banke-team' );
+                        $translated_parent_id = apply_filters( 'wpml_object_id', $parent_id->ID, 'page' );
+                    @endphp
+                    <ul class="breadcrumbs breadcrumbs--news">
+                        <li class="breadcrumbs__item">
+                            <a class="breadcrumb breadcrumb--link"
+                               href="{{ get_permalink($translated_parent_id) }}">{!! get_the_title($translated_parent_id) !!}</a>
+                        </li>
+                        <li class="breadcrumbs__item">
+                    <span class="breadcrumb">
+                        {!! get_the_title($post) !!}
+                    </span>
+                        </li>
+                    </ul>
+
                     <div class="article__heading">
                         <div class="news__date">
                             {!! $post->getDate() !!}
